cc29b08db5c6b3e003eb49f55e2a52d342eb12ca
phorian <44467015+phorian@users.noreply.github.com>
2020-06-13 20:00:28 +0100

    Update tipcalc.js

--- tipcalc.js
+++ tipcalc.js
@@ -5,12 +5,12 @@ function calculateTip(){
     let numPeople = document.getElementById("ppleshr").value;
     
     //check for zero input
-    if (billPrice === "" || sQ === 0) {
+    if (billPrice === "" || sQ == 0) {
         alert("please input values");
     return;
 }
     //validate number of people to be 1 or more than 1 but not less or empty
-    if( numPeople == "" || numPeople <= 1){
+    if( numPeople === "" || numPeople <= 1){
         numPeople = 1; 
         document.getElementById("each").style.display = "none";
        } else{
@@ -18,20 +18,23 @@ function calculateTip(){
        }
     //compute all values and calculate
     let total = (billPrice * sQ) / numPeople;
-    total = Math.round(total * 100)/ 100;
+    total = Math.round(total * 100) / 100;
     //this allows us to always have to digit after decimal point
     total = total.toFixed(2);
     //display tip result
-    document.getElementById("result").style.display = "block"
+    document.getElementById("result").style.display = "block";
     document.getElementById("tip").innerHTML = total; 
     }
 
 //hide the result on load
-document.getElementById("result").style.display = "none"
-document.getElementById("each").style.display = "none"
+document.addEventListener('DOMContentLoaded', function(){
+document.getElementById("result").style.display = "none";
+document.getElementById("each").style.display = "none";
+
 
 //using Dom to call function
-document.getElementById("calculate").addEventListener("click" calculateTip);
+document.getElementById("calculate").addEventListener('click', calculateTip)
+    });
 
 
 
